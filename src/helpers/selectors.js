bebb6a24edff753e4152f512bffd80c90da2af8d
ozggnr <ozgecaylioglu@gmail.com>
2020-10-29 20:48:20 -0400

    added new selector to get interview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,49 +1,33 @@
-import React from 'react';
 
+function getAppointmentsForDay (state, day) {
+ 
+  if (state.days.length === 0) {
+    return [];
+  } else {
+      const filteredDays = state.days.filter(item =>  item.name === day)
+      if( filteredDays.length === 0) {
+        return [];
+      }
+      
+      const list = filteredDays[0].appointments
+    
+      const filteredAppointment = list.map( (el) => {
+        return (state.appointments[el] ? state.appointments[el] : []) 
+    
+    })
+    return filteredAppointment;
+  } 
+} ;
 
-const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5]
+function getInterview (state, interview) {
+    
+    if (interview) {
+     
+      interview["interviewer"] = state.interviewers[interview.interviewer]
+    } else {
+      return null;
     }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
-    }
-  }
-};
-
-
-export function getAppointmentsForDay(state, day) {
-  const arr = []
-  const filteredDays = state.days.filter(item => item.name === day)
-  const list = filteredDays[0].appointments
-  
-  console.log(typeof (list1))
-  for (let i in list) {
-    if(state.appointments[i]) {
-      arr.push(state.appointments[i])
-    }
-  }
- return [...arr];
+  return interview;
 }
 
-console.log(typeof getAppointmentsForDay(state,"Monday"))
+export { getInterview, getAppointmentsForDay }
