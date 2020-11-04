4ed0d228672b25d18fa2df61ccde2942377f99cc
ozggnr <ozgecaylioglu@gmail.com>
2020-11-04 14:58:43 -0500

    added appointments test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,17 +1,22 @@
 /* eslint-disable no-undef */
 
 describe("Appointments", () => {
-  
-  it("should book an interview", () => {
-    cy.request("GET", "/api/debug/reset")
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
+
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
+
     cy.contains("Save").click();
+    
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
