d2156d0a37dbc8d2939f72439f9d726204757c2d
ozggnr <ozgecaylioglu@gmail.com>
2020-11-07 17:57:04 -0500

    added key for days

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,6 +5,7 @@ export default function DayList(props) {
    const days = props.days.map(day => {
     return (
     <DayListItem 
+      key={day.id}
       name={day.name} 
       spots={day.spots} 
       selected={day.name === props.day}
@@ -12,5 +13,5 @@ export default function DayList(props) {
     />
     )
   });  
-  return days;
+  return <ul>{days}</ul>;
 }
